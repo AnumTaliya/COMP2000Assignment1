ee7e8619ab7ac0753c53063ddffb4a3b0908dccf
AnumTaliya <anum.anumtaliya@students.mq.edu.au>
2026-09-09 12:26:12 +1000

    Create and write Fox.java

---
+++ COMP2000/Fox.java
@@ -0,0 +1,36 @@
+import java.awt.Color;
+import java.awt.Graphics;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Fox extends Predator {
+    public Fox(double x, double y) {
+        super(x, y, 100, 2.2, 90);
+    }
+
+    @Override
+    protected void act(World world) {
+        List<Prey> targets = new ArrayList<>();
+        targets.addAll(world.getRabbits());
+        targets.addAll(world.getMice());
+        Prey target = findNearest(targets);
+        if (target != null) {
+            moveToward(target);
+            tryEat(target);
+        } else {
+            wander();
+        }
+    }
+
+    @Override
+    public void draw(Graphics g) {
+        g.setColor(getColor());
+        g.fillOval((int) getX() - 6, (int) getY() - 4, 12, 8);
+    }
+
+    @Override
+    public Color getColor() {
+        return Color.ORANGE;
+    }
+}
