81bbb310484eebbe40843bc087d32359a890938b
AnumTaliya <anum.anumtaliya@students.mq.edu.au>
2026-09-09 12:24:47 +1000

    Create and write Hawk

---
+++ COMP2000/Hawk.java
@@ -0,0 +1,37 @@
+import java.awt.Color;
+import java.awt.Graphics;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Hawk extends Predator {
+    public Hawk(double x, double y) {
+        super(x, y, 100, 3.0, 120);
+    }
+
+    @Override
+    protected void act(World world) {
+        List<Prey> targets = new ArrayList<>();
+        targets.addAll(world.getMice());
+        targets.addAll(world.getRabbits());
+        Prey target = findNearest(targets);
+        if (target != null) {
+            moveToward(target);
+            tryEat(target);
+        } else {
+            wander();
+        }
+    }
+
+    @Override
+    public void draw(Graphics g) {
+        g.setColor(getColor());
+        g.fillPolygon(new int[]{(int) getX(), (int) getX() - 6, (int) getX() + 6},
+                      new int[]{(int) getY() - 8, (int) getY() + 6, (int) getY() + 6}, 3);
+    }
+
+    @Override
+    public Color getColor() {
+        return Color.RED;
+    }
+}
